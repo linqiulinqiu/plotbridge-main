04e31a1a654f9d457e825a3aa32acc1294ab0e44
linqiu <1131868300@qq.com>
2022-05-07 13:37:45 +0800

    revise style connect link style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,18 @@
     <el-col class="notice" :span="12" :offset="6">
       <h2>{{ $t("home-title") }}</h2>
       <el-timeline id="news-timeline">
-        <el-timeline-item v-for="item in $t('home-news')" :key="item.msg" :timestamp="item.ts">
+        <el-timeline-item
+          v-for="item in $t('home-news')"
+          :key="item.msg"
+          :timestamp="item.ts"
+        >
           <el-card class="card">{{ item.msg }}</el-card>
         </el-timeline-item>
       </el-timeline>
       <p>{{ $t("home-note") }}</p>
-      <ConnectUs />
+    </el-col>
+    <el-col :span="12" :offset="6">
+      <el-card :offset="5" :span="10" class="con-card"><ConnectUs /></el-card>
     </el-col>
   </el-col>
 </template>
@@ -28,6 +34,13 @@ export default {
   margin: 0;
   padding: 0;
 }
+.con-card {
+  /* padding: 0px 0px 20px 30%; */
+  padding-bottom: 20px;
+  background-color: rgba(255, 255, 225, 0.1);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  text-align: center;
+}
 #home {
   color: #fff;
   height: 85vh;
@@ -38,11 +51,11 @@ export default {
 }
 
 #news-timeline {
-    margin-top: 50px;
+  margin-top: 50px;
 }
 
 .card {
   color: #000;
-  background-color: rgba(255, 255, 255, 0.3)
+  background-color: rgba(255, 255, 255, 0.3);
 }
 </style>
